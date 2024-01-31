6caeeb56e75cde970c2ff777c68f2b241ad1752c
Leon Timmermans <fawaka@gmail.com>
2024-01-31 23:25:45 +0100

    Improve documentation

--- lib/Mojolicious/Plugin/Credentials.pm
+++ lib/Mojolicious/Plugin/Credentials.pm
@@ -36,21 +36,23 @@ sub register($self, $app, $config) {
 
  # Mojolicious::Lite
 
- plugin 'Credentials';
+ plugin Credentials => { keys => \@keys };
+
+ my ($password) = app->credentials->get('google');
 
  # Mojolicious
 
  sub startup {
    my $self = shift;
 
-   $self->plugin('Credentials');
+   $self->plugin(Credentials => { keys => \@keys });
  }
- 
+
 =head1 DESCRIPTION
 
 This module plugs L<Crypt::Credentials|Crypt::Credentials> into your Mojolicious application. This allows you to store credentials using only one key.
 
-Credentials can by edited using the credentials mojo command (e.g. C<./myapp.pl credentials edit google>.
+Credentials can by edited using the credentials mojo command (e.g. C<./myapp.pl credentials edit google>).
 
 =head1 CONFIGURATION
 
@@ -61,6 +63,8 @@ It takes two arguments, both optional.
 =item * keys
 
 This is the key used to encrypt the credentials. If not given this will use the environmental variable C<MOJO_CREDENTIALS_KEYS> (split on colons), and otherwise it will bail out. In both cases the key will be expected in hexadecimal form.
+
+Multiple keys are supported to aid key rotation, one would typically add the new key to the injected list, switch the store to the new key and only then remove the old key from the injection.
 
 =item * dir
 
